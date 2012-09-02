7beb6d0d85b802145c8a5c840ecd04d98fabc9d5
Sebastian Sitaru <s.sitaru@gmx.de>
2012-09-02 18:51:09 +0200

    no jquery dependence

--- chainloader.js
+++ chainloader.js
@@ -4,7 +4,7 @@
  * Copyright (C) 2012 by 1NT and FoxtrotFire
  */
  
-jQuery(document).ready(function(){
+document.addEventListener('DOMContentLoaded',function(){
 	var scriptTag = document.createElement('script');
 	scriptTag.type = 'text/javascript';
 	scriptTag.src = 'https://raw.github.com/ssitaru/foxbot/master/foxbot.js';
