cf91b2f5d7e2f5fc2ef836706fd3527dc3c0f469
foxtrotfire <foxtrot661@gmail.com>
2012-12-10 18:38:12 +0100

    Update Activator.js

--- Activator.js
+++ Activator.js
@@ -1,41 +1,44 @@
-var parser = 1;
-var o_chatcmds = {
+var Parser = false;
+
+var o_chaincmds = {
     '/chainload': {
   	f: f_load,
 		needsPerm: true,
 	}
 };
+
 function f_chainloadInit() {
 API.sendChat('/me foxbot chainloader waiting for activation!');
 API.addEventListener(API.CHAT, f_chainChat);
 } 
 function f_chainChat(data) {
-//Will work on this. It's kind of annoying as it stands and doesn't allow for cool stuff
-	if((data.type == "message") && (parser == 1) ){
-		for(var s in o_chatcmds) {
+	if(data.type == "message"){
+		for(var s in o_chaincmds) {
 			if(data.message.toString().indexOf(s) != -1) { 
-				if(o_chatcmds[s].needsPerm){
+				if(o_chaincmds[s].needsPerm){
 					if(API.getUser(data.fromID).permission.toString()>1){
-						o_chatcmds[s].f(data);
+						if(Parser){
+							o_chaincmds[s].f(data);
+						}
+						else{
+							API.sendChat('/me Bot already loaded!');
+						}
 					}
 					else{
 						API.sendChat('@'+data.from+': Im sorry Dave, but Im afraid I cant let you do that.');
 					}
-				}
-				else{
-					o_chatcmds[s].f(data);
-				}
-						   
+				}			   
 			}
-		}
+		}	
 	}
-	
 }
+
 function f_load(data){
-var parser = 0;
+var Parser = false;
 var scriptTag = document.createElement('script');
 scriptTag.type = 'text/javascript';
 scriptTag.src = 'https://raw.github.com/foxtrotfire/foxbot/master/foxbot.js';
 document.body.appendChild(scriptTag);
 }
+
 window.setTimeout(function(){f_chainloadInit();},10000);
