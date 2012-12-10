95f347dd117e02b91fa8f03e9e12cbdbfde55d86
foxtrotfire <foxtrot661@gmail.com>
2012-12-10 19:21:35 +0100

    Update Activator.js

--- Activator.js
+++ Activator.js
@@ -6,24 +6,22 @@ API.addEventListener(API.CHAT, f_chainChat);
 } 
 function f_chainChat(data) {
 	if(data.type == "message"){
-		for(var s in o_chaincmds) {
+		for(var s = '/activate') {
 			if(data.message.toString().indexOf(s) != -1) { 
-				if(o_chaincmds[s].needsPerm){
-					if(API.getUser(data.fromID).permission.toString()>1){
-						if(Activated != 1){
-							var Activated = 1;
-							var scriptTag = document.createElement('script');
-							scriptTag.type = 'text/javascript';
-							scriptTag.src = 'https://raw.github.com/foxtrotfire/foxbot/master/foxbot.js';
-							document.body.appendChild(scriptTag);
-						}
-						else{
-							API.sendChat('/me Bot already active!');
-						}
+				if(API.getUser(data.fromID).permission.toString()>1){
+					if(Activated != 1){
+						var Activated = 1;
+						var scriptTag = document.createElement('script');
+						scriptTag.type = 'text/javascript';
+						scriptTag.src = 'https://raw.github.com/foxtrotfire/foxbot/master/foxbot.js';
+						document.body.appendChild(scriptTag);
 					}
 					else{
-						API.sendChat('@'+data.from+': Im sorry Dave, but Im afraid I cant let you do that.');
+						API.sendChat('/me Bot already active!');
 					}
+				}
+				else{
+					API.sendChat('@'+data.from+': Im sorry Dave, but Im afraid I cant let you do that.');
 				}			   
 			}
 		}	
