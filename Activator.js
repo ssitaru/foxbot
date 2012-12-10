17965f7b0f0a29f72dc05abef96fb7a468ae2279
foxtrotfire <foxtrot661@gmail.com>
2012-12-10 19:35:33 +0100

    Update Activator.js

--- Activator.js
+++ Activator.js
@@ -1,4 +1,4 @@
-var Activated = false;
+var inactive = true;
 var o_chaincmds = {
     '/activate': {
 	needsPerm: true,
@@ -14,8 +14,8 @@ function f_chainChat(data) {
 		for(var s in o_chaincmds)  {
 			if(data.message.toString().indexOf(s) != -1) { 
 				if(API.getUser(data.fromID).permission.toString()>1){
-					if(Activated != true){
-						var Activated = true;
+					if(inactive){
+						var inactive = false;
 						var scriptTag = document.createElement('script');
 						scriptTag.type = 'text/javascript';
 						scriptTag.src = 'https://raw.github.com/foxtrotfire/foxbot/master/foxbot.js';
